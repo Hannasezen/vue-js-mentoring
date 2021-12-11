930ec007c07db1484d54ccca3ccf9f6291c435a3
Hanna Sezen <hanna_sezen@epam.com>
2021-12-11 19:20:28 +0300

    added Footer component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,28 @@
 <template>
   <Header />
+  <main class="main-content">
+    <Catalog />
+  </main>
+  <Footer />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import Header from "@/components/organisms/Header.vue";
+import Footer from "@/components/organisms/Footer.vue";
+import Catalog from "@/components/organisms/Catalog.vue";
 
 export default defineComponent({
   components: {
     Header,
+    Footer,
+    Catalog,
   },
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.main-content {
+  flex-grow: 1;
+}
+</style>
